27f2a41df99b3d5b1345257ce8357af5d9cac4e8
unknown <lourencovinicius00@gmail.com>
2023-10-30 21:10:54 -0300

    Adicionando configuração CORS

--- src/main/java/br/com/spring/eventasso/Configurations.java
+++ src/main/java/br/com/spring/eventasso/Configurations.java
@@ -32,7 +32,7 @@ public class Configurations {
                 .permitAll()
                 .antMatchers(HttpMethod.GET, "/home")
                 .permitAll()
-                .antMatchers(HttpMethod.GET, "/usuario")
+                .antMatchers(HttpMethod.POST, "/usuario")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and().addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
