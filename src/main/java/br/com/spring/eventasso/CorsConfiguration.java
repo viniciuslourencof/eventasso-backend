8a53d48129e6af5d5d688827d11debfd42ec474d
unknown <lourencovinicius00@gmail.com>
2023-10-30 21:01:06 -0300

    Adicionando configuração CORS

--- src/main/java/br/com/spring/eventasso/CorsConfiguration.java
+++ src/main/java/br/com/spring/eventasso/CorsConfiguration.java
@@ -1,2 +1,16 @@
-package br.com.spring.eventasso;public class Cors {
+package br.com.spring.eventasso;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("ec2-18-230-69-133.sa-east-1.compute.amazonaws.com:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+
+    }
 }
