b3f1802abb0ac3a5866a496f15302371e97bcd6f
unknown <lourencovinicius00@gmail.com>
2024-01-29 21:08:37 -0300

    commit inicial contendo estrutura base de cadastro de usuários e eventos

--- src/main/java/com/api/eventasso/configs/DateConfig.java
+++ src/main/java/com/api/eventasso/configs/DateConfig.java
@@ -1,4 +1,4 @@
-package com.api.parkingcontrol.configs;
+package com.api.eventasso.configs;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
--- src/test/java/com/api/eventasso/EventassoApplicationTests.java
+++ src/test/java/com/api/eventasso/EventassoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.api.parkingcontrol;
+package com.api.eventasso;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ParkingControlApplicationTests {
+class EventassoApplicationTests {
 
 	@Test
 	void contextLoads() {
